9bf3e49515a955ebad9024c87515ab154c7e1215
Coenie Beyers <coeniebeyers@gmail.com>
2019-02-05 14:36:11 +0000

    Can now submit new mint transactions and listen to their events

--- index.js
+++ index.js
@@ -3,11 +3,35 @@ const util = require('./util.js')
 const blockchainProxy = require('blockchain-proxy-client')({apiServerAddress: config.blockchainProxyAddress})
 const tokenProxy = require('token-proxy-client')({apiServerAddress: config.tokenProxyAddress})
 
+const mintEventList = []
+
+async function processMintEvents(){
+  const mintEvent = mintEventList.shift()
+  if(!!mintEvent){
+    console.log({mintEvent})
+  }
+}
 
 async function run(){
   try{
     const tokenContractAddress = (await tokenProxy.contractAddress()).contractAddress
-    console.log({tokenContractAddress})
+    const tokenContractABI = (await tokenProxy.getTokenContractABI()).abi
+    const mintEventPrototype = (await tokenProxy.getMintEventPrototype()).prototype
+    const filterOptions = JSON.stringify({
+      filter: {},
+      fromBlock: 0,
+      topics: [mintEventPrototype]
+    })
+
+    blockchainProxy.subscribeToContractEventsPubSub(tokenContractAddress
+      , tokenContractABI, filterOptions, function(event){
+      mintEventList.push(event)
+    })
+
+    while(true){
+      await processMintEvents()
+      await util.delay(100)
+    }
     
   } catch (err){
     console.log('ERROR in index.js->run():', err)
